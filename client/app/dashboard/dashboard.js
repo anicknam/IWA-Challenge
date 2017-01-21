d3ed8ab02784175d7fd34b1e4bfb90e202c8a582
anicknam <a.nicknam@gmail.com>
2017-01-20 16:53:49 -0800

    move processing programs to factory and inject to all controllers

--- client/app/dashboard/dashboard.js
+++ client/app/dashboard/dashboard.js
@@ -2,28 +2,10 @@ angular.module('iwa.dashboard', [])
 
 .controller('DashboardController', function ($scope, $location, DashboardPrograms) {
    
-   DashboardPrograms.getAll().then(function(res){
-    // list of programs with all given properties 
-    var rawPrograms = res.data["program_blueprints"];
-
-    // list of programs with properties of interest
-    var programs = rawPrograms.map(function(program){
-      return {"guid": program.guid,
-              "description": program.description,
-              "display_order": program.display_order,
-              "display_type": program.display_type,
-              "icon_url": program.icon_url,
-              "title": program.title,
-              "goals": program.goals
-              }
-    });
-
-    // sorted list of programs, based on display_order property 
-    $scope.programs = programs.sort(function(prog1, prog2) {
-      return (prog1.display_order > prog2.display_order) ? 1 : -1;
-    });
-
+   DashboardPrograms.getAll().then(function(programs){
+     $scope.programs = programs;
    });
+
 
    $scope.clickHandler = function(guid){
      $location.path(`details/${guid}`);
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -4,7 +4,34 @@ angular.module('iwa.services', [])
 
   var getAll = function () {
     return $http.get('./data/program_blueprints.json')
+    .then(function(res){
+      // list of programs with all given properties 
+      var rawPrograms = res.data["program_blueprints"];
+
+      // list of programs with properties of interest
+      var programs = rawPrograms.map(function(program){
+        return {"guid": program.guid,
+                "description": program.description,
+                "display_order": program.display_order,
+                "display_type": program.display_type,
+                "icon_url": program.icon_url,
+                "title": program.title,
+                "goals": program.goals
+                }
+      });
+
+      // sorted list of programs, based on display_order property 
+      var sortedPrograms = programs.sort(function(prog1, prog2) {
+        return (prog1.display_order > prog2.display_order) ? 1 : -1;
+      });
+      
+      return sortedPrograms;
+
+     });
+
   };
+
+
 
   return {
     getAll: getAll
