a24fa6dfa5c03a792638d125ccce841c885ad78f
anicknam <a.nicknam@gmail.com>
2017-01-20 19:09:40 -0800

    add goals title and incentive value for programs that have goals

--- client/app/details/details.js
+++ client/app/details/details.js
@@ -1,13 +1,36 @@
 angular.module('iwa.details', [])
 
-.controller('DetailsController', function ($scope, $routeParams, DashboardPrograms) {
-
+.controller('DetailsController', function ($scope, $routeParams, DashboardPrograms, Goals) {
+	// get guid of the clicked program
    $scope.currGuid = $routeParams.guid;
 
    DashboardPrograms.getAll().then(function(programs){
+   	 // find program with current guid from list of all programs
      $scope.currProgram = programs.filter(function(program){
        return program.guid === $scope.currGuid; 
      })[0];
+
+     // get a list of all goals
+     Goals.getAll().then(function(goals){
+     	// create a storage of all goals with guid used as key
+     	var goalsStorage = goals.reduce(function(store, goal){
+     		store[goal.guid] = {"title": goal.title, "incentive_value": goal.incentive_value}
+     		return store;
+     	}, {});
+
+
+     	console.log($scope.currProgram.goals)
+     	$scope.hasGoals = $scope.currProgram.goals.length > 0;
+     	if ($scope.hasGoals) {
+     	  $scope.currGoals = $scope.currProgram.goals.map(function(goal) {
+     	  	return goalsStorage[goal.guid];
+     	  });
+     	  console.log($scope.currGoals)
+     	}
+
+
+     })
+
    });
 
   });
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -7,7 +7,6 @@ angular.module('iwa.services', [])
     .then(function(res){
       // list of programs with all given properties 
       var rawPrograms = res.data["program_blueprints"];
-
       // list of programs with properties of interest
       var programs = rawPrograms.map(function(program){
         return {"guid": program.guid,
@@ -15,26 +14,37 @@ angular.module('iwa.services', [])
                 "display_order": program.display_order,
                 "display_type": program.display_type,
                 "icon_url": program.icon_url,
+                "incentive_description": program.incentive_description,
                 "title": program.title,
                 "goals": program.goals
                 }
       });
-
       // sorted list of programs, based on display_order property 
       var sortedPrograms = programs.sort(function(prog1, prog2) {
         return (prog1.display_order > prog2.display_order) ? 1 : -1;
       });
-      
       return sortedPrograms;
-
      });
 
   };
-
-
 
   return {
     getAll: getAll
   };
 
   })
+
+.factory('Goals', function ($http) {
+  
+  var getAll = function () {
+  	return $http.get('./data/goals.json')
+  	.then(function(res){
+  		return res.data["goals"];
+  	})
+  };
+
+  return {
+  	getAll: getAll
+  }
+
+})
